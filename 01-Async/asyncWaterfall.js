4253bb6035d183a41592eeb894dbd2aa9d7c6a88
Yongzhi Huang <yhuang@fastcompany.com>
2015-08-01 09:27:48 -0400

    added series example

--- 01-Async/asyncWaterfall.js
+++ 01-Async/asyncWaterfall.js
@@ -5,11 +5,35 @@ var github = new GitHubApi({
 	version: '3.0.0'
 });
 
+
+async.series([
+	function funcOne(callback) {
+		callback(null, 'Function one');
+	},
+	function funcTwo(callback) {
+		setTimeout(function() {
+			callback(null, 'Function two');
+		}, 2000);
+	},
+	function funcThree(callback) {
+		callback(null, 'Function three');
+	}
+], function(err, result) {
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	console.log(result);
+});
+
+
 async.waterfall([
 	function getUserAvatar(callback) {
 		github.search.users({ q:'airbnb' }, function(err, res) {
 			if (err) {
 				callback(err, null);
+				return;
 			}
 			var avatarUrl = res.items[0].avatar_url;
 			callback(null, avatarUrl);
