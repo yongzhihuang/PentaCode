6961941294a617a694ef66f62ada7e62a8d78bba
Yongzhi <yongzhihuang@yahoo.com>
2015-08-08 22:36:08 -0400

    Added Prehooks

---
+++ Prehooks/index.js
@@ -0,0 +1,14 @@
+var myFunction = function() {
+
+	if (num == 10) {
+		console.log('The number is 10')
+	}
+
+	while (num !== 10)
+		doSomething()
+
+	function doSomething() {
+		console.log('do something')
+	}
+
+}
