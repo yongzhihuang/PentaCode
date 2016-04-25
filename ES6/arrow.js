3dc193ad58f99eac4ea37555e96209eb9b3d9d55
Yongzhi Huang <yongzhihuang@yahoo.com>
2016-04-25 19:07:40 -0400

    Added ES6 Arrow function file

---
+++ ES6/arrow.js
@@ -0,0 +1,73 @@
+// Solving lexical "This" with self = this
+var myVar = {};
+
+myVar.name = 'pentacode';
+myVar.numbers = [1,2,3,4,5];
+
+myVar.printNumbers = function() {
+  var self = this;
+  this.numbers.forEach(function(number) {
+      console.log(self.name + ' counts ' + number);
+  });
+  
+}
+myVar.printNumbers();
+
+
+// Solving lexical "This" with .bind()
+var myVar = {};
+
+myVar.name = 'pentacode';
+myVar.numbers = [1,2,3,4,5];
+
+myVar.printNumbers = function() {
+
+  this.numbers.forEach(function(number) {
+      console.log(this.name + ' counts ' + number);
+  }.bind(this));
+  
+}
+myVar.printNumbers();
+
+// Solving lexical "This" with ES6 Arrow Function
+var myVar = {};
+
+myVar.name = 'pentacode';
+myVar.numbers = [1,2,3,4,5];
+
+myVar.printNumbers = function() {
+  this.numbers.forEach((number) => {
+      console.log(this.name + ' counts ' + number);
+  });
+  
+}
+myVar.printNumbers();
+
+
+
+// Arrow function short hand
+ar myVar = {};
+
+myVar.name = 'pentacode';
+myVar.numbers = [1,2,3,4,5];
+
+myVar.printNumbers = function() {
+  this.numPlusOne = this.numbers.map((number) => {return number + 1});
+  console.log(myVar.numPlusOne);
+}
+
+myVar.printNumbers();
+
+
+// Short hand method
+var myVar = {};
+
+myVar.name = 'pentacode';
+myVar.numbers = [1,2,3,4,5];
+
+myVar.printNumbers = function() {
+  this.numPlusOne = this.numbers.map(number => number + 1);
+  console.log(myVar.numPlusOne);
+}
+
+myVar.printNumbers();
