e043399dc30471340e73145c7a6f5372c270c390
Yongzhi Huang <yongzhihuang@yahoo.com>
2016-04-26 19:00:15 -0400

    Added proj file for lessons 3 and 4

---
+++ ES6/03-enhanced_object_literals_default_values.js
@@ -0,0 +1,48 @@
+/*******
+Author: Yongzhi Huang
+Website: https://www.youtube.com/pentacode
+Twitter: twitter.com/whyzhi
+********/
+
+// Enhanved Object Literals
+let foo = 'bar';
+let myObj = {
+	foo,	// same as foo: foo
+	_name: 'pentacode',
+	_age: 5,
+	whoAmI() {
+		// Set a function property 
+		console.log(`I am ${this._name} and I am ${this._age} years old`);
+	},
+	[ 'prop_' + (() => 'dynamic')() ]: 'a dynamic property'	// Dynamic key name
+}
+
+console.log(myObj.foo);
+console.log(myObj.whoAmI())
+console.log(myObj.prop_dynamic)
+
+// Default values
+function add(x,y) {
+	if (!y) {
+		y = 0;
+	}
+	console.log(x + y);
+	return x + y;
+}
+
+function add(x, y=0) {
+	console.log(x + y);
+	return x + y;
+}
+
+add(3, 4); // 7
+add(3); // 3
+
+// Evaluating default value at call time
+function pusher(value, arr = []) {
+	arr.push(value);
+	return arr;
+}
+
+let fruit = pusher('apple');
+console.log(fruit);	// ['apple']
---
+++ ES6/04-rest_and_spread_operators.js
@@ -0,0 +1,52 @@
+/*******
+Author: Yongzhi Huang
+Website: https://www.youtube.com/pentacode
+Twitter: twitter.com/whyzhi
+********/
+
+// Spread Operator
+let dietSodas = ['diet coke', 'diet pepsi', 'diet sprite'];
+let sodas = ['coke', ...dietSodas, 'sunkist', 'mountain dew'];
+console.log(sodas);	// ["coke", "diet coke", "diet pepsi", "diet sprite", "sunkist", "mountain dew"]
+
+
+function sumOfThreeValues(a, b, c) {
+	return a + b + c;
+}
+
+let numberList = [1, 2, 3];
+console.log(sumOfThreeValues(...numberList));	// 6
+
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+arr1.push(...arr2);
+console.log(arr1);	// [1, 2, 3, 4, 5, 6]
+
+
+// Rest Parameter
+// ES5
+function parameterLogger() {
+	return Array.prototype.slice.call(arguments, parameterLogger.length);
+}
+
+let params = parameterLogger(1,2,3,4,5);
+console.log(params);
+
+// ES6 with Rest parameter
+function parameterLogger(...args) {
+	// args is an array of all the arguments provided to the function
+	return args;
+}
+
+let params = parameterLogger(1,2,3,4,5);
+console.log(params);
+
+// Make functions easier to write
+function adder(numToAdd, ...args) {
+	return args.map((arg) => numToAdd + arg);
+}
+
+var ans = adder(1,2,3);	// [3, 4]
+console.log(ans);
