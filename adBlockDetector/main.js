a66b2a508d4e124b3030e0bdecef1afb3a0bdcca
Yongzhi Huang <yongzhihuang@yahoo.com>
2016-07-07 16:40:07 -0400

    Docker LEMP project files

--- adBlockDetector/main.js
+++ adBlockDetector/main.js
@@ -1,7 +1,10 @@
 if (window.showAds) {
     // Your user has no adblocker, good for them
     document.getElementById('status').innerHTML = 'No Adblocker detected.'
+    ga('send', 'event', 'User Interaction', 'Adblock', 'Disabled');
 } else {
     // You user uses adbocker, do something here
     document.getElementById('status').innerHTML = 'You have Adblocker!'
+    ga('send', 'event', 'User Interaction', 'Adblock', 'Enabled');
+
 }
