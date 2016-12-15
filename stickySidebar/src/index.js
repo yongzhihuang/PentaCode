67e12c3500bb41e72d82d4eae983823729358fe6
Yongzhi Huang <yhuang@fastcompany.com>
2016-12-14 21:58:55 -0500

    Update index.js

--- stickySidebar/src/index.js
+++ stickySidebar/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-<<<<<<< ac590930fdabb81de9e509055a52aa24b06c925d
-=======
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 >>>>>>> Added sticky bar
