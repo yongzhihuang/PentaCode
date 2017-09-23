f32849d3d73f6545273e6aa124d8e93dcb51dc46
yongzhihuang <yongzhihuang@yahoo.com>
2017-09-23 16:42:01 -0400

    solr proj code changes tutorial

--- solr/nodeapp/index.js
+++ solr/nodeapp/index.js
@@ -47,7 +47,7 @@ var client = new SolrNode({
 // const stringQuery = 'id:2';    // delete document with id 2
 // const deleteAllQuery = '*';    // delete all
 // const objectQUery = {id: 'd7497504-22d9-4a22-9635-88dd437712ff'};   // Object query
-// client.delete(stringQuery, function(err, result) {
+// client.delete(deleteAllQuery, function(err, result) {
 //   if (err) {
 //     console.log(err);
 //     return;
@@ -58,36 +58,36 @@ var client = new SolrNode({
 //-------------------------------------------------------------------------------------------
 
 // Search
-// const authorQuery = {
-//   author: 'whyzhi'
-// };
+const authorQuery = {
+  author: 'whyzhi'
+};
 
 // const genderQuery = {
 //   gender: 'Female'
 // };
 
-// // Build a search query var
-// const searchQuery = client.query()
-//   .q(genderQuery)
-//   .addParams({
-//     wt: 'json',
-//     indent: true
-//   })
-//   // .start(1)
-//   // .rows(1)
+// Build a search query var
+const searchQuery = client.query()
+  .q(authorQuery)
+  .addParams({
+    wt: 'json',
+    indent: true
+  })
+  // .start(1)
+  // .rows(1)
 
-// client.search(searchQuery, function (err, result) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
+client.search(searchQuery, function (err, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-//   const response = result.response;
-//   console.log(response);
+  const response = result.response;
+  console.log(response);
 
-//   if (response && response.docs) {
-//     response.docs.forEach((doc) => {
-//       console.log(doc);
-//     })
-//   }
-// });
+  if (response && response.docs) {
+    response.docs.forEach((doc) => {
+      console.log(doc);
+    })
+  }
+});
